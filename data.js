3dd1052d0ee8a9e0d54e5679d1164b74ba7e9d2c
Justin Khyle <152313667+Justin-Khyle@users.noreply.github.com>
2025-02-25 20:22:44 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const sculptureList = [
+    {
+        name: 'Homenaje a la Neurocirugía',
+        artist: 'Marta Colvin Andrade',
+        description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+        url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+        alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+    },
+    {
+        name: 'The Thinker',
+        artist: 'Auguste Rodin',
+        description: 'A famous bronze sculpture showing a man in deep thought, often associated with philosophy and contemplation.',
+        url: 'https://i.imgur.com/NF6VrjJ.jpg',
+        alt: 'A bronze statue of a seated man with his chin resting on his hand, deep in thought.'
+    }
+];
+
+module.exports = sculptureList;
